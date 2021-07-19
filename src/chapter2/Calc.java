c1ed3cb609d6df41143d499de9921f591c314c74
GubanovEA <genyagubanov@yandex.ru>
2021-07-19 20:33:10 +0300

    Class main was added

--- src/chapter2/Calc.java
+++ src/chapter2/Calc.java
@@ -1,28 +1,42 @@
 package chapter2;
 
+import java.util.Scanner;
+
 public class Calc {
     public static void main(String[] args) {
-            double min, avr, max;
-            double[] array = new double[9];
-            for (int i = 0; i < array.length; i++)
-                array[i] = Math.random();
-            for (int i = array.length - 1; i > 0; i--) {
-                for (int j = 0; j < i; j++) {
-                    if (array[j] > array[j + 1]) {
-                        double tmp = array[j];
-                        array[j] = array[j + 1];
-                        array[j + 1] = tmp;
-                    }
+        float min, avg, max;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите количество чисел в массиве: ");
+        int num = sc.nextInt();
+        float[] array = new float[num];
+
+        for (int i = 0; i < array.length; i++)
+            array[i] = (float) Math.random();
+
+
+
+        for (int i = 0; i < array.length; i++)
+            System.out.println(array[i]);
+
+        min = array[0];
+        avg += array[num]/array.length;
+        max = array[8];
+        System.out.println("Минимальное число в массиве - " + min);
+        System.out.println("Минимальное число в массиве - " + avg);
+        System.out.println("Минимальное число в массиве - " + max);
+
+    }
+
+    public float[] sortingArr(Float[] array) {
+        for (int i = array.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[j + 1]) {
+                    float tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
                 }
             }
-            for (int i = 0; i < array.length; i++)
-                System.out.println(array[i]);
-            min = array[0];
-            avr = array[5];
-            max = array[8];
-            System.out.println("Минимальное число в массиве - " + min);
-            System.out.println("Минимальное число в массиве - " + avr);
-            System.out.println("Минимальное число в массиве - " + max);
-
-            }
+        }
+        return array;
     }
+}
