3dadab10940b7f058c883af1c228bb5f571d11fe
GubanovEA <genyagubanov@yandex.ru>
2021-07-19 20:59:41 +0300

    Class main was added

--- src/chapter2/Calc.java
+++ src/chapter2/Calc.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class Calc {
     public static void main(String[] args) {
-        float min, avg, max;
+        float min, avg, max, sr;
+        avg = 0;
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите количество чисел в массиве: ");
         int num = sc.nextInt();
@@ -13,21 +14,6 @@ public class Calc {
         for (int i = 0; i < array.length; i++)
             array[i] = (float) Math.random();
 
-
-
-        for (int i = 0; i < array.length; i++)
-            System.out.println(array[i]);
-
-        min = array[0];
-        avg += array[num]/array.length;
-        max = array[8];
-        System.out.println("Минимальное число в массиве - " + min);
-        System.out.println("Минимальное число в массиве - " + avg);
-        System.out.println("Минимальное число в массиве - " + max);
-
-    }
-
-    public float[] sortingArr(Float[] array) {
         for (int i = array.length - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
                 if (array[j] > array[j + 1]) {
@@ -37,6 +23,20 @@ public class Calc {
                 }
             }
         }
-        return array;
+
+        for (int i = 0; i < array.length; i++)
+            System.out.println(array[i]);
+
+
+        for (int i = 0; i < array.length; i++)
+            avg += array[i] / array.length;
+
+        min = array[0];
+        max = array[num - 1];
+
+        System.out.println("Минимальное число в массиве - " + min);
+        System.out.println("Среднее значение чисел в массиве - " + avg);
+        System.out.println("Максимальное число в массиве - " + max);
+
     }
 }
